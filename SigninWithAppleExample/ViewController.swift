5a976e15c285c7afd7b52e591b10bac8019b043c
huiping_guo <huiping.guo@dena.jp>
2020-01-21 16:09:30 +0900

    project init

---
+++ SigninWithAppleExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SigninWithAppleExample
+//
+//  Created by Huiping Guo on 2020/01/21.
+//  Copyright © 2020 Huiping Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
